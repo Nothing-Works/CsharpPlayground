9b5196aacf97cea3a48392b6561e2318f43ed098
AndySong <andylauszp@gmail.com>
2020-09-29 15:48:01 +1300

    basic cookies auth

---
+++ BasicAuth/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BasicAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Authenticate()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,"Andy"),
+                new Claim(ClaimTypes.Email,"andy@infosoft.co.nz"),
+                new Claim("custom values","something I need to know."),
+            };
+
+            var identity = new ClaimsIdentity(claims, "Cookies");
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(principal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BasicAuth/Startup.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BasicAuth
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+                    o =>
+                    {
+                        o.Cookie.Name = "Andy";
+                        o.LoginPath = "/Home/Authenticate";
+                    });
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+    }
+}
