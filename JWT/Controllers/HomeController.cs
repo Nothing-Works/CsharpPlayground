27a158f3d29728ed9dc2553cea664eb5ade336e4
AndySong <andylauszp@gmail.com>
2020-10-02 13:09:20 +1300

    basic jwt

---
+++ JWT/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
+
+namespace JWT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return Ok("done");
+        }
+
+        public IActionResult Authenticate()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "id=1"),
+                new Claim("Andy", "Song"),
+            };
+
+            var key = new SymmetricSecurityKey(Convert.FromBase64String(JWTSettings.Secret));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                JWTSettings.Issuer,
+                JWTSettings.Audience,
+                claims,
+                DateTime.Now,
+                DateTime.Now.AddMinutes(30),
+                creds
+                );
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+    }
+}
---
+++ JWT/JWTSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+
+namespace JWT
+{
+    public static class JWTSettings
+    {
+        public static string Audience = "default Audience";
+        public static string Issuer = "default Issuer";
+        public static string Secret = "pintusharmaqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqweqwess";
+
+
+        public static TokenValidationParameters TokenValidationParameters => new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Issuer,
+            ValidAudience = Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(Secret))
+        };
+    }
+}
