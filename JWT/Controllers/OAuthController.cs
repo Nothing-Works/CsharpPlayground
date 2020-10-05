016a42fcd102c9f6b9097d6e2466d550d4d9d3d2
AndySong <andylauszp@gmail.com>
2020-10-05 16:32:39 +1300

    wip

---
+++ JWT/Controllers/OAuthController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT.Controllers
+{
+    public class OAuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Authorize(
+            string response_type,
+            string client_id,
+            string redirect_uri,
+            string scope,
+            string state
+        )
+        {
+            var query = new QueryBuilder { { "redirect_uri", redirect_uri }, { "state", state } };
+
+            return View(model: query.ToString());
+        }
+
+
+        [HttpPost]
+        public IActionResult Authorize(
+            string username,
+            string password,
+            string redirect_uri,
+            string state
+        )
+        {
+            const string code = "OAuth Code";
+            var query = new QueryBuilder { { "code", code }, { "state", state } };
+
+            return Redirect($"{redirect_uri}{query}");
+        }
+
+        public async Task<IActionResult> Token(
+            string grant_type,
+            string code,
+            string redirect_uri,
+            string client_id
+            )
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "id=1"),
+                new Claim("Andy", "Song"),
+            };
+
+            var key = new SymmetricSecurityKey(Convert.FromBase64String(JWTSettings.Secret));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                JWTSettings.Issuer,
+                JWTSettings.Audience,
+                claims,
+                DateTime.Now,
+                DateTime.Now.AddMinutes(30),
+                creds
+            );
+
+            var responseObject = new { access_token = new JwtSecurityTokenHandler().WriteToken(token), token_type = "Bearer" };
+
+            var responseJson = JsonConvert.SerializeObject(responseObject);
+            var responseBytes = Encoding.UTF8.GetBytes(responseJson);
+
+            await Response.Body.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+            return Redirect(redirect_uri);
+        }
+    }
+}
---
+++ JWTClient/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTClient
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(o =>
+                {
+                    o.DefaultChallengeScheme = "OAuthServer";
+                    o.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    o.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOAuth("OAuthServer", o =>
+                {
+                    o.CallbackPath = new PathString("/Home/Callback");
+                    o.ClientId = "Provider:ClientId";
+                    o.ClientSecret = "Provider:ClientSecret";
+                    o.AuthorizationEndpoint = "https://localhost:44363/OAuth/Authorize";
+                    o.TokenEndpoint = "https://localhost:44363/OAuth/Token";
+                    o.SaveTokens = true;
+
+                    o.Events = new OAuthEvents
+                    {
+                        OnCreatingTicket = context =>
+                        {
+                            var accessToken = context.AccessToken;
+                            var payload = accessToken.Split('.')[1];
+                            var bytes = Convert.FromBase64String(payload.PadRight(payload.Length + (payload.Length * 3) % 4, '='));
+                            var jsonPayload = Encoding.UTF8.GetString(bytes);
+                            var claims = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonPayload);
+
+                            foreach (var (key, value) in claims)
+                            {
+                                context.Identity.AddClaim(new Claim(key, value));
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+    }
+}
